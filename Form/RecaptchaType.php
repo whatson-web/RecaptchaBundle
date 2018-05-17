01b41656819fdb308fb102342bd65b29a1265625
JF <jf@whatson-web.com>
2018-05-17 10:43:36 +0200

    form validation

--- Form/RecaptchaType.php
+++ Form/RecaptchaType.php
@@ -2,11 +2,24 @@
 
 namespace WH\RecaptchaBundle\Form;
 
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class RecaptchaType extends AbstractType
 {
+    private $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(
@@ -17,5 +30,31 @@ class RecaptchaType extends AbstractType
                 ],
             ]
         );
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->addEventListener(
+            FormEvents::PRE_SUBMIT,
+            [
+                $this,
+                'onPreSubmit',
+            ]
+        );
+    }
+
+    public function onPreSubmit(FormEvent $event)
+    {
+        $request = Request::createFromGlobals();
+
+        if ($request->request->has('g-recaptcha-response')) {
+            $gRecaptchaResponse = $request->request->get('g-recaptcha-response');
+
+            $request->request->remove('g-recaptcha-response');
+
+            if (!$this->container->get('wh_recaptcha.validator')->isResponseValid($gRecaptchaResponse)) {
+                $event->getForm()->addError(new FormError('Vérification anti-spam échouée'));
+            }
+        }
     }
 }
